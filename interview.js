f8b32b773467647eff03eb99c20f4731b018b2a8
manav083 <manavjain083@gail.com>
2022-11-02 15:29:58 +0530

    changes

---
+++ interview.js
@@ -0,0 +1,27 @@
+ 
+// let arr = [1,2,4,3,5,6,7,5,6,4,3,7]
+// let arr2 = [];
+
+// for(let i = 0; i<arr.length; i++){
+//     for(let j = i + 1; j<arr.length; j++){
+//         if(arr[i] === arr[j]){
+//             if(arr2[i] != arr[j]){
+//             arr2.push(arr[i]);
+//         }
+//             // arr.length = arr.length-1;
+
+//         }
+//     }
+    
+// }
+
+// let str = "This is a string";
+// let str2 = "";
+
+// for(let i = str.length - 1; i >= 0; i--){
+//     // console.log(str[i])
+//     str2 = str2 + str[i];
+// }
+
+
+// console.log(str2);
